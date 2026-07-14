d051294ec2946963811992b11e0b8f161edf46a9
m-emin <mehmet.emin.akdogan.30@gmail.com>
2026-07-15 00:19:40 +0300

    GameState autoload and live score UI added

--- label.gd
+++ label.gd
@@ -1,4 +1,4 @@
-extends Area2D
+extends Label
 
 
 # Called when the node enters the scene tree for the first time.
@@ -8,9 +8,4 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
-
-
-func _on_body_entered(body: Node2D) -> void:
-	GameState.add_point()
-	queue_free()
+	text = "Score: " + str(GameState.score)
--- player.gd
+++ player.gd
@@ -20,4 +20,5 @@ func _process(delta: float) -> void:
 	if Input.is_action_pressed("move_up"):
 		direction.y -= 1
 	
+	direction = direction.normalized()
 	position += direction * speed * delta
