33f441e2ae42fd716115078846a2549727c417b9
m-emin <mehmet.emin.akdogan.30@gmail.com>
2026-07-15 01:16:38 +0300

    Codebase organized

--- game_state/GameState.gd
+++ game_state/GameState.gd
@@ -7,4 +7,4 @@ func add_point():
 	score += 1
 	print("Score is now: ", score)
 	if score >= total_coins:
-		get_tree().call_deferred("change_scene_to_file", "res://win_screen.tscn")
+		get_tree().call_deferred("change_scene_to_file", "res://game_state/win_screen.tscn")
