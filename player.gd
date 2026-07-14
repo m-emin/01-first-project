d148732d91ff21cb02e71016f5444fa4df9fd457
m-emin <mehmet.emin.akdogan.30@gmail.com>
2026-07-14 16:36:28 +0300

    Coin collection logic added

--- player.gd
+++ player.gd
@@ -1,6 +1,5 @@
 extends Node2D
 
-# Global Variables
 @export var speed = 200
 
 # Called when the node enters the scene tree for the first time.
@@ -10,17 +9,15 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	
 	var direction = Vector2.ZERO
 	
-	if Input.is_action_pressed("move_up"):
-		direction.y -= 1
-	if Input.is_action_pressed("move_down"):
-		direction.y += 1
 	if Input.is_action_pressed("move_right"):
 		direction.x += 1
 	if Input.is_action_pressed("move_left"):
 		direction.x -= 1
+	if Input.is_action_pressed("move_down"):
+		direction.y += 1
+	if Input.is_action_pressed("move_up"):
+		direction.y -= 1
 	
-	direction = direction.normalized()
 	position += direction * speed * delta
