97e5186aebe8a49608a0da6a5cd658d3327ff545
m-emin <mehmet.emin.akdogan.30@gmail.com>
2026-07-16 13:44:40 +0300

    Win / Lose scenes + replayability added

--- game_state/GameState.gd
+++ game_state/GameState.gd
@@ -1,12 +1,27 @@
 extends Node
 
 @export var win_screen: PackedScene = preload("res://game_state/win_screen.tscn")
+@export var lose_screen: PackedScene = preload("res://game_state/lose_screen.tscn")
 
 var score: int = 0
 var total_coins: int = 0
+var total_traps: int = 0
 
 func add_point():
 	score += 1
 	print("Score is now: ", score)
 	if score >= total_coins:
 		get_tree().call_deferred("change_scene_to_packed", win_screen)
+		self.call_deferred("reset_score")
+		
+func sub_point():
+	score -= 1
+	print("Score is now: ", score)
+	if score < 0:
+		get_tree().call_deferred("change_scene_to_packed", lose_screen)
+		self.call_deferred("reset_score")
+		
+func reset_score():
+	print("Current score is:", score)
+	score = 0
+	print("Score resetted to:", score)
--- main/main.gd
+++ main/main.gd
@@ -4,8 +4,11 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var coins: Array[Node] = get_tree().get_nodes_in_group("coins")
+	var traps: Array[Node] = get_tree().get_nodes_in_group("traps")
 	GameState.total_coins = coins.size()
+	GameState.total_traps = traps.size()
 	print("Total coins found: ", GameState.total_coins)
+	print("Total traps found: ", GameState.total_traps)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
